1a515a6985e8436b3130d71cdb441b726fa5ac0a
Luqman Arifin <l.arifin.siswanto@gmail.com>
2017-06-21 15:50:20 +0900

    add ms computer vision

--- ms-computer-vision/coba.sh
+++ ms-computer-vision/coba.sh
@@ -1,5 +1,5 @@
 curl -v -X POST "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/ocr?language=unk&detectOrientation=true" \
 -H "Content-Type: application/json" \
 -H "Ocp-Apim-Subscription-Key: " \
---data-ascii '{"url": "http://imgur.com/fbavkw0.jpg"}'
+-F 'image=@102-TAC-1.jpg'
 
